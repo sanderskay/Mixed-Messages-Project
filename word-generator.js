970d27d4e6dd335c50cba26c985b07761c184aa3
Julian Sanders-Kay <sanderskay@hotmail.com>
2020-12-03 13:18:44 -0500

    Add Phrases

--- word-generator.js
+++ word-generator.js
@@ -1,1 +1,16 @@
-console.log("Teeessstt");
+const firstPhrase = ["Get out of bed,", "Hurry up,", "Come on,"];
+
+const secondPhrase = [
+  " you jabroni,",
+  " it's a beautiful day,",
+  " there are spiders everywhere,",
+];
+
+const thirdPhrase = [
+  " let's make the best of it.",
+  " we gotta run!",
+  " don't ruin this for me.",
+];
+
+// function will return firstPhrase etc each using Math.random
+// to return a random line from each array
